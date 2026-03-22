8dd7322f782974289906fb939de7ababff483274
Paulo <paulo.peluso15@gmail.com>
2026-03-22 16:13:50 -0300

    refactor: separa funcao utilitaria em utils

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
+from utils import saudacao
+
 def main():
-    print("Projeto funcionando.")
+    print(saudacao('Paulo'))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ src/app/utils.py
@@ -0,0 +1,2 @@
+def saudacao(nome: str) -> str:
+    return f'Ola, {nome}! Projeto organizado funcionando.'
